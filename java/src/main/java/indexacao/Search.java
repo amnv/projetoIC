0b3ec7a1eba245a34465e5457a8108fbb0b08743
Allyson Manoel <amnv@cin.ufpe.br>
2018-10-10 10:00:58 -0300

    código para indexar arquivos

---
+++ java/src/main/java/indexacao/Search.java
@@ -0,0 +1,70 @@
+package indexacao;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+public class Search {
+
+	 //directory contains the lucene indexes
+    private static final String INDEX_DIR = "../data/luceneIndex";
+ 
+    public static void main(String[] args) throws Exception
+    {
+    	Scanner in = new Scanner(System.in);
+    	String searchText = in.nextLine();
+    	
+    	
+        //Create lucene searcher. It search over a single IndexReader.
+        IndexSearcher searcher = createSearcher();
+         
+        //Search indexed contents using search term
+        TopDocs foundDocs = searchInContent(searchText, searcher);
+         
+        //Total found documents
+        System.out.println("Total Results :: " + foundDocs.totalHits);
+         
+        //Let's print out the path of files which have searched term
+        for (ScoreDoc sd : foundDocs.scoreDocs)
+        {
+            Document d = searcher.doc(sd.doc);
+            System.out.println("Path : "+ d.get("path") + ", Score : " + sd.score);
+        }
+        in.close();
+    }
+     
+    private static TopDocs searchInContent(String textToFind, IndexSearcher searcher) throws Exception
+    {
+        //Create search query
+        QueryParser qp = new QueryParser("contents", new StandardAnalyzer());
+        Query query = qp.parse(textToFind);
+         
+        //search the index
+        TopDocs hits = searcher.search(query, 10);
+        return hits;
+    }
+ 
+    private static IndexSearcher createSearcher() throws IOException
+    {
+        Directory dir = FSDirectory.open(Paths.get(INDEX_DIR));
+         
+        //It is an interface for accessing a point-in-time view of a lucene index
+        IndexReader reader = DirectoryReader.open(dir);
+         
+        //Index searcher
+        IndexSearcher searcher = new IndexSearcher(reader);
+        return searcher;
+    }
+}
