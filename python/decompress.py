029b3f40897128b863faedef0d8c9d375b3bf786
Allyson Manoel <amnv@cin.ufpe.br>
2019-02-21 01:41:37 -0300

    Ajustes no algoritmo de decompressão para pegar dados da pasta err_files

--- python/decompress.py
+++ python/decompress.py
@@ -2,11 +2,12 @@ import glob
 import gzip
 import shutil
 
-files = glob.glob("../data/*.txt.gz")
+files = glob.glob("../data/err_files/*.txt.gz")
 for fname in files:
     #defene file name
    ## print(fname)
     fname_out = fname.split(".gz")[0]
+    fname_out = fname_out.replace("/err_files/", "/")
     print("Extraindo arquivo ", fname_out)
 
     with gzip.open(fname, 'rb') as f_in:
